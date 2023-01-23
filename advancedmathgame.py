966bdc8fb60b2d02e45d73f185dea73b833bb613
shiftyHungary <fehervari23@gmail.com>
2023-01-23 21:27:29 +0100

    nevezéktan javítása

--- advancedmathgame.py
+++ advancedmathgame.py
@@ -8,12 +8,12 @@ rounds = 0
 
 print("Matek teszt!")
 while True: 
-    gameMode = int(input("Válassz feladatot! Összeadás (1), kivonás(2), szorzás(3) vagy osztás(4)?: "))
+    gameMode = int(input("Válassz tesztet! Összeadás (1), kivonás(2), szorzás(3) vagy osztás(4)?: "))
 
     if gameMode == 1:
         #összeadás
         rounds += 1
-        print(rounds, ". feladat!")
+        print(rounds, ". teszt!")
         for i in range (1,11):
             a = random.randint(1,100)
             b = random.randint(1,100)
@@ -28,7 +28,7 @@ while True:
     elif gameMode == 2:
         #kivonás
         rounds += 1
-        print(rounds, ". feladat!")
+        print(rounds, ". teszt!")
         for j in range (1,11):
             c = random.randint(1,100)
             d = random.randint(1,100)
@@ -43,7 +43,7 @@ while True:
     elif gameMode == 3:
         #szorzás
         rounds += 1
-        print(rounds, ". feladat!")
+        print(rounds, ". teszt!")
         for k in range (1,11):
             e = random.randint(1,12)
             f = random.randint(1,12)
@@ -58,7 +58,7 @@ while True:
     elif gameMode == 4:
         #osztás
         rounds += 1
-        print(rounds, ". feladat!")
+        print(rounds, ". teszt!")
         for l in range (1,11):
             g = random.randint(1,100)
             h = random.randint(1,100)
